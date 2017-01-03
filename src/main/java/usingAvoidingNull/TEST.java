89b0802b663a48d43dab2f358343753664cd7c52
szd1007 <szd1007@gmail.com>
2017-01-03 20:19:02 +0800

    udpate

--- src/main/java/usingAvoidingNull/TEST.java
+++ src/main/java/usingAvoidingNull/TEST.java
@@ -1,7 +1,14 @@
 package usingAvoidingNull;
 
+import java.util.Optional;
+
 /**
  * Created by shangzhidong on 2017/1/3.
  */
 public class TEST {
+    public static void main(String args[]){
+        Object a  =1;
+        Optional<Object> possible = Optional.of(a);
+        System.out.println(possible.isPresent());
+    }
 }
