4517d698376b355db929e0a1951538ae8485da94
szd1007 <szd1007@gmail.com>
2017-01-11 19:43:44 +0800

    update

---
+++ src/main/java/collections/CollectionUtilitiesExplainedTest.java
@@ -0,0 +1,8 @@
+package collections;
+
+/**
+ * Created by shangzhidong on 2017/1/11.
+ */
+public class CollectionUtilitiesExplainedTest {
+
+}
--- src/main/java/collections/NewCollectionTypesExplainedTest.java
+++ src/main/java/collections/NewCollectionTypesExplainedTest.java
@@ -1,16 +1,21 @@
 package collections;
 
-import com.google.common.collect.HashMultiset;
-import com.google.common.collect.Multiset;
+import com.google.common.collect.*;
+
+import java.util.Collection;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by zm on 17/1/10.
+ * https://github.com/google/guava/wiki/NewCollectionTypesExplained
  */
 public class NewCollectionTypesExplainedTest {
 
     public static void main(String[] args) {
 
-        /**multiset test*/
+        /**multiset test */
         Multiset<String> multiset = HashMultiset.create();
         multiset.add("a");
         multiset.add("b");
@@ -20,7 +25,50 @@ public class NewCollectionTypesExplainedTest {
         for(String s:multiset){
             System.out.println(s);
         }
-        multiset.
+        System.out.println(multiset.count("a"));
+        multiset.remove(null);
+
+
+        /**multiMap
+         * better than:  Map<K,List<V>> (ArrayListMultimap)   Map<K,Set<V>>(HashMultimap)
+         * 不用处理null的情况
+         *
+         */
+        ArrayListMultimap<String,Integer> multimap =  ArrayListMultimap.create();
+        List<Integer> key = multimap.get("n");
+        multimap.put("a",1);
+        multimap.put("b",1);
+        multimap.put("a",2);
+        System.out.println(key.size());
+        //multimap.asMap();
+        for(Map.Entry<String, Collection<Integer>> entry:multimap.asMap().entrySet()){
+            entry.getKey();
+            List<Integer>value = (List<Integer>) entry.getValue();
+            System.out.println(value);
+        }
+        System.out.println("@@@@@@@@@@@@@");
+        for(Map.Entry<String,Integer> entry :multimap.entries()){
+            System.out.println(entry.getKey()+":"+entry.getValue());
+        }
+
+        System.out.println("2222222222222");
+        System.out.println(multimap.containsKey("a"));
+        multimap.removeAll("a");
+        System.out.println(multimap.containsKey("a"));
+
+
+
+        /**
+         * BiMap  一一对应  value值也必须唯一
+         */
+        System.out.println("**********BiMap********************");
+        BiMap<String,String> bimap = HashBiMap.create();
+        bimap.put("id1","a");
+//        bimap.put("id2","a");//throw Exception
+//        bimap.putIfAbsent("id2","a");
+        bimap.put("id3","c");
+        System.out.println(bimap.inverse().keySet());//inverse方法
+        System.out.println("**********BiMap********************");
 
     }
 }
