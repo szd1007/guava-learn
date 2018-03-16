f9620efb2b44aaa9513cdda2923cde416edc9db7
szd1007 <szd1007@gmail.com>
2018-03-16 19:30:37 +0800

    py

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,9 +4,13 @@
 public class Test {
     volatile long defaultVaule ;
 
-
+    static void f1(int a) {
+        System.out.println(a);
+    }
     public static void main(String[] args) {
         //System.out.println(new Test().defaultVaule);
         System.out.println("ddd");
+        Integer a = null;
+        f1(a);
     }
 }
