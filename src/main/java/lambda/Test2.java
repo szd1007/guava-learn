92fddcf3e9e2375b93bb7103d566b16e87f4a89c
szd1007 <szd1007@gmail.com>
2018-05-01 00:20:13 +0800

    文本处理增加进度条功能

---
+++ src/main/java/lambda/Test2.java
@@ -0,0 +1,33 @@
+package lambda;
+
+import com.sun.istack.internal.NotNull;
+
+public class Test2 {
+
+//    @FunctionalInterface
+    interface DoStuff{
+        boolean isGood(int value);
+
+        default void xx(int b) {
+            System.out.println(b);
+        }
+
+    }
+
+    static void doSomthing( DoStuff stuff) {
+        System.out.println(stuff.isGood(41));
+        System.out.println(stuff.isGood(22));
+    }
+    public static void main(String[] args) {
+        doSomthing(new DoStuff() {
+
+            @Override public boolean isGood(int value) {
+                return value == 41;
+            }
+        });
+        doSomthing(answer -> answer == 41);
+
+
+    }
+
+}
---
+++ src/main/python/readFileWithBar.py
@@ -0,0 +1,15 @@
+input="asn.txt"
+output=input+".sh"
+logName="zzmerchant_info.log.2018-04-2*"
+file = open(input)
+outFile = open(output, 'w')
+count=len(file.readlines())
+outFile.write("#!/bin/sh\n")
+#outFile.write(str(count))
+file2 = open(input)
+i=0
+for line in file2:
+    i += 1
+    outFile.write("cat "+logName+" |grep "+line.strip()+" |grep \"ZZMerchantController:41\" |head -n1 >>more \n")
+    outFile.write("printf \"process asncode["+line.strip()+"]   "+str(i*100/count)+"%%\\r\"\n")
+outFile.close
