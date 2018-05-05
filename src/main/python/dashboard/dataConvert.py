cd81af66da2e4ee5182c3b8c2b284443cd3c2ee9
shangzhidong <shangzhidong@zhuanzhuan.com>
2018-05-05 13:30:38 +0800

    格式化python代码

--- src/main/python/dashboard/dataConvert.py
+++ src/main/python/dashboard/dataConvert.py
@@ -1,31 +1,25 @@
 import time
 import datetime
 
-input="details"
-output=input+".sql"
-table="recsys_kpi_logtype_cn_new"
-
-
+input = "details"
+output = input + ".sql"
+table = "recsys_kpi_logtype_cn_new"
 
 file = open(input)
-outfile = open(output,'w')
-outfile.write("delete from "+table+" where 1=1 ; \r\n")
-tmp_line="insert into "+table +" (type_1,logtype,logtype_cn,`desc`)values('"
-#for rn in rowName:
+outfile = open(output, 'w')
+outfile.write("delete from " + table + " where 1=1 ; \r\n")
+tmp_line = "insert into " + table + " (type_1,logtype,logtype_cn,`desc`)values('"
+# for rn in rowName:
 #    tmp_line+=(rn +",")
-#tmp_line += " values ("+timFormat+","
-    
+# tmp_line += " values ("+timFormat+","
+
 for line in file:
     la = line.split("\t")
-    if(la[3].strip()=="logtype"):
-        sqlLine = tmp_line +"pc','"+la[0].strip()+"','"+la[1].strip()+"','"+la[2].strip()+"');"
+    if (la[3].strip() == "logtype"):
+        sqlLine = tmp_line + "pc','" + la[0].strip() + "','" + la[1].strip() + "','" + la[2].strip() + "');"
     else:
-        sqlLine = tmp_line +"page','"+la[0].strip()+"','"+la[1].strip()+"','"+la[2].strip()+"');"
-    
-    outfile.write(sqlLine+"\r\n")
-    
-    
-outfile.close
+        sqlLine = tmp_line + "page','" + la[0].strip() + "','" + la[1].strip() + "','" + la[2].strip() + "');"
 
-    
-    
+    outfile.write(sqlLine + "\r\n")
+
+outfile.close
--- src/main/python/readFile.py
+++ src/main/python/readFile.py
@@ -1,10 +1,10 @@
-input="asn.txt"
-output=input+".sql"
+input = "asn.txt"
+output = input + ".sql"
 
 file = open(input)
 outFile = open(output, 'w')
 
 for line in file:
-    outFile.write(""+line.strip()+"'  order by create_time desc limit 1\" > "+line.strip())
+    outFile.write("" + line.strip() + "'  order by create_time desc limit 1\" > " + line.strip())
 
 outFile.close
