970f224917398f6200dce075f33cc5c38102c924
normalAdam <szd1007@gmail.com>
2018-10-16 10:09:18 +0800

    continue outer

--- src/main/java/thecompletereferenc/ControlState.java
+++ src/main/java/thecompletereferenc/ControlState.java
@@ -2,6 +2,19 @@ package thecompletereferenc;
 
 public class ControlState {
     public static void main(String[] args) {
+        outer:
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j > i) {
+                    System.out.println();
+                    continue outer;
+                }
+                System.out.print(" " + (i * j));
+            }
+        }
+        System.out.println();
+    }
+    public static void main2(String[] args) {
         int a, b;
         for(a=1,b=4; a<b; a++) {
             System.out.println(a+";"+b);
