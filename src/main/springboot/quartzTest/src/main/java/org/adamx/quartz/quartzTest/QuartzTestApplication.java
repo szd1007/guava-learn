37e7e8ed8c0ced5f9aa40299aca50dbd42393b93
shangzhidong <shangzhidong@zhuanzhuan.com>
2018-10-25 10:25:52 +0800

    up

--- src/main/springboot/quartzTest/src/main/java/org/adamx/quartz/quartzTest/QuartzTestApplication.java
+++ src/main/springboot/quartzTest/src/main/java/org/adamx/quartz/quartzTest/QuartzTestApplication.java
@@ -1,12 +1,15 @@
 package org.adamx.quartz.quartzTest;
 
 import org.quartz.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
-public class QuartzTestApplication {
+public class QuartzTestApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuartzTestApplication.class, args);
@@ -26,4 +29,12 @@ public class QuartzTestApplication {
 		return TriggerBuilder.newTrigger().forJob(sampleJobDetail())
                              .withIdentity("sampleTrigger").withSchedule(scheduleBuilder).build();
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(">>>>>>>>>>>>>>>>>> input args...." + (args.length > 0 ? args[0] : "null"));
+        TimeUnit.SECONDS.sleep(6);
+        System.out.println(">>>>>>>>>>>>>>>>>>> run success");
+
+    }
 }
