9f556d0173a09e917d0d0331b87cef618d945891
shangzhidong <shangzhidong@zhuanzhuan.com>
2018-10-26 13:40:47 +0800

    ChainException

--- src/main/java/thecompletereferenc/Exception.java
+++ src/main/java/thecompletereferenc/Exception.java
@@ -20,6 +20,19 @@ public class Exception {
 
     public static void main(String[] args) {
         divideByZero();
+        System.out.println(returnAfterFinally());
+        System.out.println(returnAfterFinallyWithObj());
+        System.out.println(returnAfterFinallyObj().b);
+
+        try {
+            ChainExcDemo.demoProc();
+        } catch (java.lang.NullPointerException e) {
+            //display top level exception
+            System.out.println("Caught: " + e);
+
+            //display cause exception
+            System.out.println("Original cause: " + e.getCause());
+        }
     }
 
     public static void divideByZero(){
@@ -33,5 +46,50 @@ public class Exception {
             System.out.println("ex");
             e.printStackTrace();
         }
+    }
+
+    static int returnAfterFinally() {
+        int b = 0;
+        try{
+            return b;
+        }finally {
+            b = 2;
+        }
+    }
+    static int returnAfterFinallyWithObj() {
+        Ea a = new Ea();
+        try{
+            a.b = 1;
+            return a.b;
+        }finally {
+            a.b = 3;
+        }
+    }
+    static Ea returnAfterFinallyObj() {
+        Ea a = new Ea();
+        try{
+            a.b = 1;
+            //return 的时候已经赋值， 后续不能改变这个值或者引用
+            return a;
+        }finally {
+            a.b = 3;
+            a = null;
+        }
+    }
+}
+
+class Ea{
+    int b ;
+}
+
+//链式异常
+class ChainExcDemo{
+    static void demoProc(){
+        //create an exception
+        NullPointerException e = new NullPointerException("top layer");
+
+        //addd a cause
+        e.initCause(new ArithmeticException("cause"));
+        throw e;
     }
 }
