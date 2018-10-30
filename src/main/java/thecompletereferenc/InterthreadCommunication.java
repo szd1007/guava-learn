6401f2055c8d54802965ec107898f248c0bcc795
shangzhidong <shangzhidong@zhuanzhuan.com>
2018-10-30 10:28:44 +0800

    interthread communication 生产者消费者

---
+++ src/main/java/thecompletereferenc/InterthreadCommunication.java
@@ -0,0 +1,94 @@
+package thecompletereferenc;
+
+/**
+ * @author shangzhidong@zhuanzhuan.com
+ * @date 2018-10-30 09:23
+ */
+public class InterthreadCommunication {
+
+}
+
+//参考ReentrantLock
+class Q {
+    int n;
+    boolean valueSet = false;
+    synchronized int get() {
+        while (!valueSet) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println("InterruptedException caught");
+            }
+        }
+        System.out.println("Got: " + n);
+        valueSet = false;
+        notify();
+//        notifyAll();
+        return n;
+    }
+
+    synchronized void put(int n) {
+        //wait 通过循环判断一个条件是否就绪进行使用
+        while (valueSet) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println("InterruptedException caught");
+            }
+        }
+        this.n = n;
+        valueSet = true;
+        System.out.println("Put: " + n);
+        notify();
+    }
+}
+
+class Producer implements Runnable {
+    Q q;
+    Thread t;
+
+    Producer(Q q) {
+        this.q = q;
+        t = new Thread(this, "Producer");
+    }
+
+    @Override
+    public void run() {
+        int i = 0;
+        while (true) {
+            q.put(i++);
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    Q q;
+    Thread t ;
+
+    Consumer(Q q) {
+        this.q = q;
+        t = new Thread(this, "Consumer");
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            q.get();
+        }
+    }
+}
+
+class PCFixed {
+
+    public static void main(String[] args) {
+        Q q = new Q();
+        Producer p = new Producer(q);
+        Consumer c = new Consumer(q);
+
+        //Start the threads.
+        p.t.start();
+        c.t.start();
+
+        System.out.println("Press Control-C to stop");
+    }
+}
