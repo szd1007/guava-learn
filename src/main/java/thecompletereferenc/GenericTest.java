342677bed7953a8b78cdd5a143276b44a25127cd
shangzhidong <shangzhidong@zhuanzhuan.com>
2018-11-09 18:29:37 +0800

    generic

--- src/main/java/thecompletereferenc/GenericTest.java
+++ src/main/java/thecompletereferenc/GenericTest.java
@@ -1,19 +1,64 @@
 package thecompletereferenc;
 
+import com.google.common.collect.Lists;
+
 /**
  * @author shangzhidong@zhuanzhuan.com
  * @date 2018-11-08 13:29
  */
 public class GenericTest implements Runnable{
 
+    //泛型方法， 放在returnType之前
+    //determine if an object is in an array
+    //加了V 这样 T或者T的子类都可以判断,都使用T也可以，只要都满足T的定义也行
+    static <T extends Comparable<T>, V extends T> boolean isIn(T x, V[] y) {
+        for (V v : y) {
+            if (x.equals(v)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Integer nums[] = { 1, 3 };
+        if (isIn(1, nums)) {
+            System.out.println("in");
+        }
+
+        String strs[] = { "122", "3" };
+        if (isIn("3", strs)) {
+            System.out.println();
+        }
+
+        geb[] bb = new geb[0];
+        gea aa = new gea();
+        geb b = new geb();
+        //V定义指定 可以是gea 或者子类.r
+        isIn(aa, bb);
+        isIn(b, bb);
+
+    }
+
+    static class  gea implements Comparable {
+
+        @Override
+        public int compareTo(Object o) {
+            return 0;
+        }
+    }
+    static class geb extends gea{
+
+    }
+
     @Override
     public void run() {
 
     }
 
-    public static void main(String[] args) {
-        GenTest<GenericTest> gg = new GenTest<>();
-    }
+//    public static void main(String[] args) {
+//        GenTest<GenericTest> gg = new GenTest<>();
+//    }
 }
 
 /**
@@ -52,6 +97,7 @@ class Stats<T extends Number>{
 
     //使用通配符 ,此处代表任意的一个 t exteds number 对象， 类型限制看定义
     boolean sameAvg(Stats<?> ob) {
+//    <X extends T>  boolean sameAvg(Stats<X> ob) {
         if (average() == ob.average()) {
             return true;
         }
@@ -69,6 +115,8 @@ class Stats<T extends Number>{
         Stats<Integer> si = new Stats<Integer>(new Integer[]{1, 2});
         Stats<Double> ss = new Stats<Double>(new Double[]{1.0, 2.0});
 
+        //raw type
+        Stats xx = new Stats(null);
 
         System.out.println(si.sameAvg(ss));
 
@@ -111,9 +159,35 @@ class GCoordi <T extends Gga>{
         System.out.println(gg.ob[0].a);
     }
     //T 限制类型， 通配符向上匹配，限制子类
+    //通配符只能在引用参数中使用，不能直接<?>使用
+    //代表的意思是 Ggb 或者 其子类
     public void printB(GCoordi<? extends Ggb> obj) {
         System.out.println(obj.ob[0].c);
     }
+
+    //代表意思    Gga或者他的父类
+    public void printSu(GCoordi<? super Gga> a) {
+
+    }
+
+    void test() {
+        Gga a = new Gga(1, 2);
+        GCoordi<Gga> gg = new GCoordi<>(new Gga[0]);
+        //super  参数类本身
+        printSu(gg);
+        //传递的参数必须在声明类型中包含，
+//        GCoordi<Object> ggcc = new GCoordi<>(new Object[0]);
+
+        //error  要是ggb和子类
+//        printB(gg);
+        GCoordi<Ggb> ggb = new GCoordi<>(new Ggb[0]);
+        printB(ggb);
+
+
+        printSu(gg);
+
+    }
+
 }
 
 
