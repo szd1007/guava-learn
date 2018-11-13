2e8b81dce9990952c39fddf08a432a8e36419e6e
shangzhidong <shangzhidong@zhuanzhuan.com>
2018-11-13 13:45:58 +0800

    lambda effect final  throw ex

---
+++ src/main/java/thecompletereferenc/EmptyArrayException.java
@@ -0,0 +1,14 @@
+package thecompletereferenc;
+
+import java.lang.Exception;
+
+/**
+ * @author shangzhidong@zhuanzhuan.com
+ * @date 2018-11-13 13:24
+ */
+public class EmptyArrayException extends Exception {
+
+    EmptyArrayException() {
+        super("Array Empty");
+    }
+}
--- src/main/java/thecompletereferenc/Lambda.java
+++ src/main/java/thecompletereferenc/Lambda.java
@@ -71,3 +71,165 @@ class LambdaDemo3 {
         }
     }
 }
+
+interface NumericFunc {
+
+    int func(int n);
+}
+
+class BlockLambdaDemo {
+
+    public static void main(String[] args) {
+        //This block lambda computes the factorial of an int value.
+        //int result =1;
+        //外部定义的变量要是final的才可以
+        NumericFunc factorial = (n -> {
+            int result = 1;
+
+            for (int i = 1; i <= n; i++) {
+                result *= i;
+            }
+            return result;
+        });
+
+        System.out.println("The factorial of 3 is " + factorial.func(3));
+        System.out.println("The factorial of 5 is " + factorial.func(5));
+    }
+}
+
+//a block lambda that reverses the characters in a string.
+interface StringFunc {
+
+    String func(String string);
+}
+
+class BlockLambdaDemo2 {
+
+    public static void main(String[] args) {
+        //This block lambda reverses the characters in a string .
+        StringFunc reverse = (str -> {
+            String result = "";
+            int i ;
+            for(i = str.length()-1; i >= 0; i--) {
+                result += str.charAt(i);
+            }
+            return result;
+        });
+        System.out.println("Lambda reversed is " + reverse.func("Lambda"));
+        System.out.println("Expression reversed is " + reverse.func("Expression"));
+    }
+}
+
+//Use a generic functional interface with lambda expressions.
+
+//A generic functional interface.
+interface SomeFunc<T> {
+
+    T func(T t);
+}
+
+class GenericFunctionalInterfaceDemo {
+
+    public static void main(String[] args) {
+        //Use a String-based version of SomeFunc.
+        SomeFunc<String> reverse = (str) -> {
+            String res = "";
+            int i;
+
+            for (int j = str.length() - 1; j >= 0; j--) {
+                res += str.charAt(j);
+            }
+            return res;
+        };
+
+        System.out.println("Lambda reversed is " + reverse.func("Lambda"));
+        System.out.println("Expression reversed is " + reverse.func("Expression"));
+
+        //Now, use an Integer-based version of SomeFunc.
+        SomeFunc<Integer> factorial = (n) -> {
+            int res = 1;
+            for (int i = 1; i <= n; i++) {
+                res *= i;
+            }
+            return res;
+        };
+
+        System.out.println("The factorial of 3 is " + factorial.func(3));
+        System.out.println("The factorial of 5 is " + factorial.func(5));
+
+
+    }
+}
+
+
+//lambda当作参数进行传递
+class LambdaAsArgumentsDemo {
+
+    static String stringOp(SomeFunc<String> fun, String string) {
+        return fun.func(string);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(stringOp(String::toUpperCase, "hello world"));
+
+        String outStr = stringOp((str) -> {
+            String res = "";
+            int i;
+            for (int j = str.length() - 1; j >= 0; j--) {
+                res += str.charAt(j);
+            }
+            return res;
+        }, "xxx123");
+
+        System.out.println(outStr);
+    }
+}
+
+
+interface DoubleNumericArrayFunc {
+    //lambda抛出的检查异常需要是在functionalInterface中显示声明的异常
+    double func(double [] n) throws EmptyArrayException;
+}
+
+class LambdaExceptionDemo {
+
+    public static void main(String[] args) throws EmptyArrayException {
+        double[] values = { 1.0, 2.0, 3.0, 4.0 };
+
+        //This block lambda computes the average of an array of doubles.
+        DoubleNumericArrayFunc average = (n) -> {
+            double sum = 0;
+            if (n.length == 0) {
+                throw new EmptyArrayException();
+            }
+            for (int i = 0; i < n.length; i++) {
+                sum += n[i];
+            }
+            return sum / n.length;
+        };
+        System.out.println("The average is " + average.func(values));
+        System.out.println("The average is " + average.func(new double[0]));
+
+    }
+}
+
+class VarCapture {
+
+    //成员变量可以改
+    private int a;
+
+    void test() {
+        int enclosingLocal = 10;
+        SomeFunc<Integer> func = (n) -> {
+            a = n + enclosingLocal;
+            a++;
+            // lambda的封装范围的局部变量必须是 effect final（创建后不能更改)
+            //            enclosingLocal ++;
+            return a;
+        };
+
+        //这样也不行 导致变量不是effect final
+//        enclosingLocal ++;
+    }
+}
+
