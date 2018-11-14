53dd5c26c4acc33d5738f8a80e5f0004a23773e6
normalAdam <szd1007@gmail.com>
2018-11-15 07:56:35 +0800

    done lambda

--- src/main/java/thecompletereferenc/Lambda222.java
+++ src/main/java/thecompletereferenc/Lambda222.java
@@ -1,5 +1,7 @@
 package thecompletereferenc;
 
+import java.util.function.Function;
+
 /**
  * @author shangzhidong@zhuanzhuan.com
  * @date 2018-11-14 13:36
@@ -37,5 +39,76 @@ class ConstructorRefDemo {
 
         System.out.println(obj1);
         System.out.println(obj2);
+    }
+}
+
+interface MyFunc33<R, T> {
+    R func(T n);
+}
+//A simple generic class.
+class Myclass3<T> {
+    private T val;
+
+    Myclass3(T val) {
+        this.val = val;
+    }
+
+    T getVal() {
+        return val;
+    }
+}
+
+class Myclass4 {
+    String str;
+
+    Myclass4(String s) {
+        str = s;
+    }
+
+    String getVal() {
+        return str;
+    }
+}
+
+class ConstrucotrRefDemo3{
+
+    //工厂方法创建类，所有满足 Myfunc33  R func(T n); 类型构造函数的类都可以这么创建
+    static <R, T> R myClassFactory(MyFunc33<R, T> func33, T t) {
+        return func33.func(t);
+    }
+
+    public static void main(String[] args) {
+        MyFunc33<Myclass3<Double>, Double> doubleMyFunc33 = Myclass3<Double>::new;
+        Myclass3<Double> my1 = myClassFactory(doubleMyFunc33, 1.0);
+
+        MyFunc33<Myclass4, String> yyFunc44 = Myclass4::new;
+        Myclass4 myclass4 = myClassFactory(yyFunc44, "sdfs");
+
+        System.out.println(my1.getVal());
+
+        System.out.println(myclass4.getVal());
+    }
+}
+
+
+/*
+  java  预定义functional interface
+    function<t,r>
+    Predicate<T>
+
+ */
+
+class UseFunctionInterfaceDemo {
+    public static void main(String[] args) {
+        Function<Integer, Integer> factorial = (x)->{
+            int res = 1;
+        for (int i = 1; i <= x; i++) {
+            res *= i;
+        }
+        return res;
+        };
+
+        System.out.println("the factorial of 3 " + factorial.apply(3));
+
     }
 }
