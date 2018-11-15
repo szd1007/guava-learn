cb01cf60d868e0310832420f894820b9199b7c3e
shangzhidong <shangzhidong@zhuanzhuan.com>
2018-11-15 21:27:26 +0800

    module demo

---
+++ src/main/java/thecompletereferenc/module/mymodapp/appsrc/appfuncs/module-info.java
@@ -0,0 +1,5 @@
+//Module definition for the funcitons module.
+module appfuncs {
+    //Exports the package appfuncs.simplefuncs.
+    exports appfuncs.simplefuncs;
+}
--- src/main/java/thecompletereferenc/module/mymodapp/appsrc/appstart/appstart/mymodappdemo/MyModAppDemo.java
+++ src/main/java/thecompletereferenc/module/mymodapp/appsrc/appstart/appstart/mymodappdemo/MyModAppDemo.java
@@ -1,10 +1,15 @@
-package appstart.mymodaddemo;
+package appstart.mymodappdemo;
 
 import appfuncs.simplefuncs.SimpleMathFuncs;
 
 public class MyModAppDemo {
 
     public static void main(String[] args) {
+        if (SimpleMathFuncs.isFactor(2, 10)) {
+            System.out.println("2 is factor of 10");
+        }
+        System.out.println("Smallest factor common to both 35 and 105 is " + SimpleMathFuncs.lcf(35, 105));
+        System.out.println("Largest facotr common to both 35 and 105 is" + SimpleMathFuncs.gcf(35, 105));
 
     }
 }
