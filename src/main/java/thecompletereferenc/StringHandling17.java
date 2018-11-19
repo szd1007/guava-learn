c8711a4415dfec12dca5101b4decf05a61173629
shangzhidong <shangzhidong@zhuanzhuan.com>
2018-11-19 13:34:24 +0800

    getCharsS

--- src/main/java/thecompletereferenc/StringHandling17.java
+++ src/main/java/thecompletereferenc/StringHandling17.java
@@ -1,5 +1,9 @@
 package thecompletereferenc;
 
+import com.google.common.base.Charsets;
+
+import java.nio.charset.Charset;
+
 public class StringHandling17 {
 
     public static void main(String[] args) {
@@ -7,6 +11,43 @@ public class StringHandling17 {
 
         String s = new String(c);
         String s1 = new String(c, 1, 1);
+
+        byte[] ascii = { 22, 66, 67, 68, 69, 70 };
+
+        String as1 = new String(ascii);
+        System.out.println(as1);
+
+        String as2 = new String(ascii, 2, 3);
+        System.out.println(as2);
+
+        //兼容ascii码，utf8
+        String as3 = new String(ascii, Charsets.UTF_8);
+        System.out.println(as3);
+
+        //valueOf 重载函数在连接字符串时被调用
+        String cont = "this is " + String.valueOf(9);
+        String cont2 = "this is " + 9;
+
+        //getChars
+        getChars();
+
+        //getBytes
+        System.out.println(s.getBytes(Charsets.UTF_8));
+
+        //toCharArray
+        char[] s_Array = s.toCharArray();
+
+    }
+
+    static void getChars() {
+        String s = "This is a demo of the getChars method.";
+        int start = 10;
+        int end   = 14;
+        char buf[] = new char[end - start];
+
+        s.getChars(start, end, buf, 0);
+        System.out.println(buf);
+
 
     }
 }
