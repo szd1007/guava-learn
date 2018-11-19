ac2f2808d9f6cca0cc33162d4fda010b743937d9
normalAdam <szd1007@gmail.com>
2018-11-19 09:28:43 +0800

    stringHandling

---
+++ src/main/java/thecompletereferenc/StringHandling17.java
@@ -0,0 +1,12 @@
+package thecompletereferenc;
+
+public class StringHandling17 {
+
+    public static void main(String[] args) {
+        char[] c = {'a', 'c'};
+
+        String s = new String(c);
+        String s1 = new String(c, 1, 1);
+
+    }
+}
