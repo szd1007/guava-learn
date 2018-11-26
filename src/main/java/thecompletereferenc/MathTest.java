5546364d745d0a84b0b94483d4b63ee8076cb666
shangzhidong <shangzhidong@zhuanzhuan.com>
2018-11-26 14:10:48 +0800

    strictMatchTest

--- src/main/java/thecompletereferenc/MathTest.java
+++ src/main/java/thecompletereferenc/MathTest.java
@@ -1,10 +1,28 @@
 package thecompletereferenc;
 
+import org.junit.Test;
+
 public class MathTest {
 
     public static void main(String[] args) {
         int a =1, b=2;
         //抛出异常 when overflow
         System.out.println(Math.addExact(a, b));
+    }
+
+    @Test
+    public void testToRadiansToDegrees() {
+        double theta = 120.0;
+        System.out.println(theta + " degrees is " + Math.toRadians(theta) + " radians");
+
+        theta = 1.312;
+        System.out.println(theta + " radians is " + Math.toDegrees(theta) + " degrees.");
+
+    }
+
+    @Test
+    public void testStrictMath() {
+        int abs = StrictMath.abs(-1);
+        System.out.println(abs);
     }
 }
