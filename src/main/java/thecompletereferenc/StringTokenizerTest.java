547e406779b6b2242ff30b4d1a1011181d1019e2
shangzhidong <shangzhidong@zhuanzhuan.com>
2018-12-12 13:29:56 +0800

    tokenizerTest

---
+++ src/main/java/thecompletereferenc/StringTokenizerTest.java
@@ -0,0 +1,25 @@
+package thecompletereferenc;
+
+import java.util.StringTokenizer;
+
+/**
+ * @author shangzhidong@zhuanzhuan.com
+ * @date 2018-12-12 13:25
+ */
+public class StringTokenizerTest {
+
+    static String in = "title=Java: The Complete Reference;"
+            + "author= Schildt;"
+            + "publisher=Oracle Press;"
+            + "copyright=2018";
+
+    public static void main(String[] args) {
+        StringTokenizer st = new StringTokenizer(in, "=;");
+        while (st.hasMoreTokens()) {
+            String key = st.nextToken();
+            String value = st.nextToken();
+            System.out.println(key + "\t" + value);
+        }
+
+    }
+}
