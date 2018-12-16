6eb6faf84a8c50bddce26fbc162626499807961a
normalAdam <szd1007@gmail.com>
2018-12-17 07:48:21 +0800

    format test

--- src/main/java/thecompletereferenc/FormatTest.java
+++ src/main/java/thecompletereferenc/FormatTest.java
@@ -9,6 +9,7 @@ import java.util.Date;
 import java.util.Formatter;
 
 public class FormatTest {
+    private static final Formatter fsmt = new Formatter(System.out);
     public static void main(String[] args) {
         Formatter fmt = new Formatter();
 
@@ -92,4 +93,54 @@ public class FormatTest {
         fmt.format("%.15s%n", "Formatting with Java is now easy.");
 
     }
+
+    @Test
+    public void testLeftJustify() {
+
+        //right justify default
+        fsmt.format("|%10.2f|\n", 123.123);
+
+        //now left justify
+        fsmt.format("|%-10.2f|\n", 123.123);
+
+        //cause a + flag to be shown before a positive number
+        fsmt.format("|%-10.2f|\n", 123.123);
+    }
+
+    @Test
+    public void testSpace() {
+        fsmt.format("% d\n", 100);
+        fsmt.format("% d\n", -200);
+        fsmt.format("%+d\n", 300);
+        fsmt.format("%d", -400);
+    }
+
+    @Test
+    public void testCommaFlag() {
+        fsmt.format("%,d\n", 123456789);
+        fsmt.format("%,.2f\n", 42342355252.23424);
+
+        fsmt.format("%#X\n", 250);
+        fsmt.format("%X\n", 250);
+        fsmt.format("%x\n", 250);
+
+        //指定参数位置 1$ 指定是第一个参数
+        fsmt.format("%d in hex is %1$x\n", 255);
+    }
+
+    @Test
+    public void testRelativeIndex() {
+        //< 表示前面用到过的参数
+        Calendar cal = Calendar.getInstance();
+        fsmt.format("Today is day %te of %<tB, %<tY", cal);
+    }
+
+    @Test
+    public void testTryWithResource() {
+        try (Formatter fmt = new Formatter()) {
+            fmt.format("auto close %s is easy ", "Java");
+            System.out.println(fmt);
+        }
+    }
+
 }
