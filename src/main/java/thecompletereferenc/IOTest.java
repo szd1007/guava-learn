d59fc8143529eb33efbe90fe38115550a424b81b
shangzhidong <shangzhidong@zhuanzhuan.com>
2018-12-18 21:06:28 +0800

    IOTest

---
+++ src/main/java/thecompletereferenc/IOTest.java
@@ -0,0 +1,26 @@
+package thecompletereferenc;
+
+import org.junit.Test;
+
+import java.io.*;
+
+/**
+ * @author shangzhidong@zhuanzhuan.com
+ * @date 2018-12-18 20:52
+ */
+public class IOTest {
+
+    @Test
+    public void testStreamClose() {
+
+        //如果有checkedException，也必须显示catch
+        try (InputStream inputStream = new FileInputStream(new File("test"))) {
+            //do something
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
