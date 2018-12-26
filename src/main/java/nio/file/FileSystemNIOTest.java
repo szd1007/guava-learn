dafdc04d5822b6731f49f27322c0af764c47533c
normalAdam <szd1007@gmail.com>
2018-12-27 07:41:27 +0800

    nio File op

--- src/main/java/nio/file/FileSystemNIOTest.java
+++ src/main/java/nio/file/FileSystemNIOTest.java
@@ -6,12 +6,66 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 /**
  * @author shangzhidong@zhuanzhuan.com
  * @date 2018-12-26 10:23
  */
 public class FileSystemNIOTest {
+
+
+    class MyFileVisitor extends SimpleFileVisitor<Path> {
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            System.out.println(file);
+            return FileVisitResult.CONTINUE;
+        }
+    }
+
+    @Test
+    public void dirTreeList() {
+        String dirName = "/Users/zm/gongsiConf/";
+        System.out.println("Directory tree starting with " + dirName +":\n");
+
+        try {
+            Files.walkFileTree(Paths.get(dirName), new MyFileVisitor());
+        } catch (IOException exec) {
+            System.out.println("I/O Error");
+        }
+    }
+    @Test
+    public void dirListTest() {
+        String dirName = "/Users/zm/gongsiConf/";
+        //Obtain and manage a directory stream within a try block.
+
+        //过滤文件  通配符
+//        try (DirectoryStream<Path> dirstm = Files.newDirectoryStream(Paths.get(dirName), "*.{xml")) {
+//            try (DirectoryStream<Path> dirstm = Files.newDirectoryStream(Paths.get(dirName))) {
+        //过滤文件  接口方式， demo过滤可写操作文件
+        try (DirectoryStream<Path> dirstm = Files.newDirectoryStream(Paths.get(dirName),
+                Files::isWritable)) {
+            System.out.println("Directory of " + dirName);
+
+            //Because DirectoryStream implements Iterable, we
+            //can use a "foreach" loop to display the directory
+            for (Path entry : dirstm) {
+                BasicFileAttributes attributes = Files.readAttributes(entry, BasicFileAttributes.class);
+
+                if(attributes.isDirectory())
+                    System.out.print("<DIR> ");
+                else
+                    System.out.print("      ");
+                System.out.println(entry.getName(3));
+            }
+        } catch (InvalidPathException e) {
+            System.out.println("Path Error " + e);
+        }catch (NotDirectoryException e) {
+            System.out.println(dirName + " is not a directory");
+        }catch (IOException e) {
+            System.out.println("I/O Error: " + e);
+        }
+    }
 
     @Test
     public void testPath() throws IOException {
