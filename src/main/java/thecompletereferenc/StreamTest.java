9903cee5b185dae8af8a9fa584336dc92015f424
shangzhidong <shangzhidong@zhuanzhuan.com>
2019-01-04 20:08:30 +0800

    Stream base

---
+++ src/main/java/thecompletereferenc/StreamTest.java
@@ -0,0 +1,71 @@
+package thecompletereferenc;
+
+import com.google.common.collect.Lists;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * @author shangzhidong@zhuanzhuan.com
+ * @date 2019-01-04 13:25
+ */
+public class StreamTest {
+
+    public static void main(String[] args) {
+        int a[] = { 1, 3, 2 };
+        //IntStream DoubleStream LongStream 都是适用于基本数据类型
+        System.out.println(IntStream.of(a).sum());
+
+        //也是基本类型
+//        OptionalInt.of();
+
+    }
+
+    @Test
+    public void testBasic() {
+        List<Integer> myList = Lists.newArrayList(7, 18, 10, 24, 17, 5);
+        System.out.println("Original list: " + myList);
+
+        //Obtain a Stream to the array list.
+        Stream<Integer> myStream = myList.stream();
+
+        //Obtain the minimum value by use of min().
+        //max(), isPresent(), and get().
+        Optional<Integer> minVal = myStream.min(Integer::compare);
+
+        minVal.ifPresent(integer -> System.out.println("Minimum value: " + integer));
+
+        //Must obtain a new stream because previous call to min()
+        //is a terminal operation that consumed the stream.
+        myStream = myList.stream();
+        Optional<Integer> maxVal = myStream.max(Integer::compare);
+        maxVal.ifPresent(x -> System.out.println("Maximum value: " + maxVal.get()));
+
+        //Sort the stream by use of sorted().
+        Stream<Integer> sortedStream = myList.stream().sorted();
+
+        //Display the sorted stream by use of forEach().
+        System.out.println("Sorted stream: ");
+        sortedStream.forEach(x -> System.out.print(x + " "));
+        System.out.println();
+
+        //Display only the odd values by use of filter().
+        Stream<Integer> oddVals = myList.stream().sorted().filter(x -> x % 2 == 1);
+        System.out.println("Odd values: ");
+        oddVals.forEach(x -> System.out.print(x + " "));
+        System.out.println();
+
+        //Display only the odd values that are greater than 5. Notice that
+        //two filter operations are pipelined.
+
+        oddVals = myList.stream().filter(x -> x % 2 == 1).filter(x -> x > 5);
+        System.out.println("Odd values greater than 5: ");
+        oddVals.forEach(x -> System.out.print(x + " "));
+        System.out.println();
+
+    }
+}
