4e4cb48daba0c1325610eae0cfc8a6417a21c6b3
normalAdam <szd1007@gmail.com>
2019-01-17 09:18:02 +0800

    refactoring starting point

---
+++ src/main/java/refactoring/chp1_starting_point/Movie.java
@@ -0,0 +1,31 @@
+package refactoring.chp1_starting_point;
+
+public class Movie {
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+
+    private String title;
+    private int priceCode;
+
+    public Movie(String title, int priceCode) {
+        this.priceCode = priceCode;
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPriceCode() {
+        return priceCode;
+    }
+
+    public void setPriceCode(int priceCode) {
+        this.priceCode = priceCode;
+    }
+}
