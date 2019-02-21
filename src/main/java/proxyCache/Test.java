77bb7c6949e4e7569908c8de6ca072cf88e1dffe
shangzhidong <shangzhidong@zhuanzhuan.com>
2019-02-21 13:27:47 +0800

    Extract Method

--- src/main/java/proxyCache/Test.java
+++ src/main/java/proxyCache/Test.java
@@ -6,27 +6,34 @@ import com.google.common.collect.Sets;
 import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Map;
+import java.util.StringJoiner;
 
 public class Test {
 
 
 
     public static void main(String[] args) throws FileNotFoundException {
+
+        StringJoiner joiner = new StringJoiner(" , ", " , ", " ");
+        joiner.add("1=2");
+//        joiner.add("x='y'");
+        System.out.println(joiner.toString());
+//    }
 //        System.out.println("hello world");
 //        PrintStream ps=new PrintStream(new FileOutputStream("work"));
 //        System.setOut(ps);
 //        System.setErr(ps);
-        ITest test = new TestImpl();
-        Map<String, RedisCache.KeyGen> keyGenMap = Maps.newHashMap();
-        test = RedisCache.Factory.getCacheWithinTenMin(Sets.newHashSet("getPerson", "getPersonMap"), test, keyGenMap);
-        //取第一参数  然后toString
-        keyGenMap.put("getPerson", (RedisCache.KeyGen<String>) arg -> arg);
+//        ITest test = new TestImpl();
+//        Map<String, RedisCache.KeyGen> keyGenMap = Maps.newHashMap();
+//        test = RedisCache.Factory.getCacheWithinTenMin(Sets.newHashSet("getPerson", "getPersonMap"), test, keyGenMap);
+//        //取第一参数  然后toString
+//        keyGenMap.put("getPerson", (RedisCache.KeyGen<String>) arg -> arg);
+////
+////        System.out.println(test.add(1, 2));
+//        Map<String, Person> people = test.getPersonMap("adam好");
+//        Person per = people.get("adam好3");
 //
-//        System.out.println(test.add(1, 2));
-        Map<String, Person> people = test.getPersonMap("adam好");
-        Person per = people.get("adam好3");
-
-        System.out.println(per);
+//        System.out.println(per);
 
     }
 
--- src/main/java/refactoring/RfExtractMethod.java
+++ src/main/java/refactoring/RfExtractMethod.java
@@ -1,4 +1,153 @@
 package refactoring;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/**
+ * Motivation
+ *  1 拆解长代码段方法
+ *  2 当一段代码需要注释其作用时，代码拆分成新方法
+ *
+ *  if extracting improves clarity,do it , even if the name is longer than the code you have extracted.
+ *
+ *  Mechanics
+ *  1 创建新方法，命名要说出方法要做什么，而不是如何做
+ *  2 当需要返回多个结果到当前方法而无法应用此模式时
+ *    先进行  @see RfSplitTemporaryVariable   @see RfReplaceTempWithQuery @see RfReplaceMethodWithMethodObject
+ */
 public class RfExtractMethod {
+    // before  //////////////////////////////////////////////////////////////////////////
+    /**
+     *
+     * @param amount
+     */
+    void printOwing(double amount) {
+
+        printBanner();
+
+        //print details
+        System.out.println("name:" + "shan");
+        System.out.println("amount" + amount);
+
+
+    }
+
+    // after   ////////////////////////////////////////////////////////////////////////////////
+
+
+    void printOwingA(double amount) {
+        printBanner();
+        printDetails(amount);
+    }
+
+    private void printDetails(double amount) {
+        System.out.println("name:" + "shan");
+        System.out.println("amount" + amount);
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////
+
+    private void printBanner() {
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////复杂示例//////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+@SuppressWarnings("all")
+class RemDemo {
+//////////////////////////////////////////////////////////////////////////////////
+    void printOwing() {
+        List<String> names = Lists.newArrayList("a", "ben", "d", "c");
+        double charLens = 0;
+
+        //print banner
+        System.out.println("*****************");
+        System.out.println("****print owing");
+        System.out.println("*****************");
+
+        //calculate len
+        for (String name : names) {
+            charLens += name.length();
+        }
+
+        //print details
+        for (String name : names) {
+            System.out.println("name: " + name);
+            System.out.println("len:" + name.length());
+        }
+    }
+//////////////////////////////////////////////////////////////////////////////////
+    void printOwing2() {
+        List<String> names = Lists.newArrayList("a", "ben", "d", "c");
+        double charLens = 0;
+
+        //print banner
+        printBanner();
+
+        //calculate len
+        charLens = getLen(names);
+
+        //print details
+        printDetail(charLens);
+    }
+
+    private void printDetail(double charLens) {
+        System.out.println("name: " + "joe");
+        System.out.println("len:" + charLens);
+    }
+
+    private double getLen(List<String> names) {
+        double charLens = 0;
+        for (String name : names) {
+            charLens += name.length();
+        }
+        return charLens;
+    }
+
+    private void printBanner() {
+        System.out.println("*****************");
+        System.out.println("****print owing");
+        System.out.println("*****************");
+    }
+//////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////
+    //目标  像读注释一样来读代码
+    void printOwing3() {
+        List<String> names = Lists.newArrayList("a", "ben", "d", "c");
+        //print banner
+        printBanner();
+        //print details
+        printDetail(getLen(names));
+    }
+
+
+//////////////////////////////////////////////////////////////////////////////////
+
+    //////////////////////////////////////////////////////////////////////////////////
+    //目标  像读注释一样来读代码
+    void printOwing4() {
+
+        //变量只被getLen使用，直接迁移到getLen方法就行
+//        List<String> names = Lists.newArrayList("a", "ben", "d", "c");
+
+        //print banner
+        printBanner();
+        //print details
+        printDetail(getLen());
+    }
+
+    private double getLen() {
+        List<String> names = Lists.newArrayList("a", "ben", "d", "c");
+        double charLens = 0;
+        for (String name : names) {
+            charLens += name.length();
+        }
+        return charLens;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////
+
 }
