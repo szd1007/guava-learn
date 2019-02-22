c16d332815c9a72cb3f4ffd55da76b76841d01f7
shangzhidong <shangzhidong@zhuanzhuan.com>
2019-02-22 11:46:57 +0800

    repalce temp with query

--- src/main/java/refactoring/RfReplaceTempWithQuery.java
+++ src/main/java/refactoring/RfReplaceTempWithQuery.java
@@ -5,13 +5,13 @@ package refactoring;
  * Motivation
  * 减少方法长度， 减少{@link RfExtractMethod} 传递参数
  *
- * @see RfSplitTemporaryVariable
- * @see RfSeparateQueryFromModifier
+ * @see RfSplitTemporaryVariable （如果一个临时变量设置超过一次）
+ * @see RfSeparateQueryFromModifier （如果查询中要修改对象 not free of side effects)
  *
  */
 @SuppressWarnings("all")
 public class RfReplaceTempWithQuery {
-    double quantity=0,itemPrice=0;
+    static double quantity=0,itemPrice=0;
     ////////////////////////////////////////////////////
     double test() {
         double basePrice = quantity * itemPrice;
@@ -20,7 +20,9 @@ public class RfReplaceTempWithQuery {
         } else
             return basePrice * 0.98;
     }
-    //////////////////////////////////////////////////////
+    ///////////带来的性能损耗是否值得///////////////////////////////////////////
+    // nine times out of ten, it won't matter, you will ofen find more powerful optimizations,
+    //which you would have missed without refactoring. If worse comes to worse, it is easy back to temp.
     double test2() {
         if (basePrice() > 1000) {
             return basePrice() * 0.95;
@@ -28,7 +30,36 @@ public class RfReplaceTempWithQuery {
             return basePrice() * 0.98;
     }
 
-    private double basePrice() {
+    private static double basePrice() {
         return quantity * itemPrice;
+    }
+
+    ////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////
+
+    static class inTest1{
+
+        double getPrice() {
+            double basePrice = quantity * itemPrice;
+            double discountFactor ;
+            if (basePrice > 1000) {
+                discountFactor = 0.95;
+            }else {
+                discountFactor = 0.85;
+            }
+            return basePrice * discountFactor;
+        }
+////////////////////////////////////////////////////////////////////
+        double getPrice2() {
+            return basePrice() * discountFactor();
+        }
+
+        private double discountFactor() {
+            //使用查询替代临时变量，减少了extractMethod时传递的参数。更加清晰
+            if (basePrice() > 1000) {
+                return 0.95;
+            }
+            return 0.98;
+        }
     }
 }
