975933ed3b7b7129639c4b68e221463bee53b160
normalAdam <szd1007@gmail.com>
2019-03-08 07:48:52 +0800

    repalce record with data class

---
+++ src/main/java/refactoring/RfReplaceRecordWithDataClass.java
@@ -0,0 +1,14 @@
+package refactoring;
+/**
+ *  你需要在一个传统的编程环境中与记录结构体交互的时候。 为这个记录创建一个数据对象。
+ *
+ *
+ *  Mechanics
+ *  1. 创建一个类来代表记录
+ *  2. 对应field为private，并添加get set方法
+ *
+ *  这时候你有一个dumb data object, 暂时没有行为。但是后续的重构很可能会加上行为属性。
+ */
+
+public class RfReplaceRecordWithDataClass {
+}
