a8553b8e24f1bee4dd31a45ccb3124a0698e50f0
normalAdam <szd1007@gmail.com>
2019-03-14 09:29:48 +0800

    replace conditional with polymorphism

---
+++ src/main/java/refactoring/Chp9SimplifyingConditionalExpression/ReplaceConditionalWithPolyMorphism.java
@@ -0,0 +1,9 @@
+package refactoring.Chp9SimplifyingConditionalExpression;
+
+/**
+ * 你有一个条件判断根据不同的类型来选择不同的行为的时候
+ *   将每个分支下沉到子类，当前方法改写成abstract的。
+ *   @see refactoring.ReplaceTypeCodeWithStateOrStrategy
+ */
+public class ReplaceConditionalWithPolyMorphism {
+}
