48df26ac455f70de8cd48ec09868c97506ebd465
shangzhidong <shangzhidong@zhuanzhuan.com>
2019-03-25 20:45:27 +0800

    add parameter / remover parameter

---
+++ src/main/java/refactoring/chp10_makingMethodCallsSimpler/RfAddParameter.java
@@ -0,0 +1,22 @@
+package refactoring.chp10_makingMethodCallsSimpler;
+
+/**
+ * 一个方法需要更多调用者的信息的时候。添加参数到方法列表上
+ * <p>
+ * 如果有替代方法尽量不要用这个（long paraList means bad smell code ）。
+ * {@link RfIntroduceParameterObject }
+ *
+ *
+ * getContract()
+ *------------------------------------------------------------------------------------
+ * getContract(Date date)
+ *
+ * @author shangzhidong@zhuanzhuan.com
+ * @date 2019-03-25 20:22
+ */
+public class RfAddParameter {
+
+
+
+
+}
---
+++ src/main/java/refactoring/chp10_makingMethodCallsSimpler/RfRemoveParameter.java
@@ -0,0 +1,16 @@
+package refactoring.chp10_makingMethodCallsSimpler;
+
+/**
+ * 当一个参数不再使用的时候，及时的去掉这个参数
+ *
+ * getContract(Date date)
+ * ----------------------------------------------
+ * getContract()
+ *
+ *
+ * @author shangzhidong@zhuanzhuan.com
+ * @date 2019-03-25 20:34
+ */
+public class RfRemoveParameter {
+
+}
