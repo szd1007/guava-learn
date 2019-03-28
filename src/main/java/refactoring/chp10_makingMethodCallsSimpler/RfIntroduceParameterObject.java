533e704d88901111065a39bcbd483908bbb5ad2c
shangzhidong <shangzhidong@zhuanzhuan.com>
2019-03-28 09:43:22 +0800

    introduce parameter object

--- src/main/java/refactoring/chp10_makingMethodCallsSimpler/RfIntroduceParameterObject.java
+++ src/main/java/refactoring/chp10_makingMethodCallsSimpler/RfIntroduceParameterObject.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 /**
  * 你有一组参数列表通常一起出现的时候。
- * 将他们替换成一个对象
- * 这样的好处是可以发现新的重构方法，可以讲关联的行为移到新的类中 {@link refactoring.RfExtractMethod }
+ * 将他们替换成一个对象.这个过程也是新增对象的过程（属性加行为)
+ * 这样的好处是可以发现新的重构方法，可以将关联的行为移到新的类中 {@link refactoring.RfExtractMethod }
  * {@link refactoring.RfMoveMethod}
  *
  */
@@ -36,11 +36,74 @@ public class RfIntroduceParameterObject {
             double getFlowBetween(Date start, Date end) {
                 double result = 0;
                 for (Entry entry : entryList) {
-//                    if(entry.getCharDate())
+                    if (entry.getCharDate().equals(start) || entry.getCharDate().equals(end) || (entry.getCharDate().after(start) && entry
+                            .getCharDate().before(end))) {
+                        result += entry.getValue();
+                    }
                 }
                 return result;
             }
         }
+
+        void testBefore() {
+            Account account = new Account();
+            Date start = null, end = null;
+            account.getFlowBetween(start, end);
+        }
+    }
+
+    class After{
+        class DateRange{
+            private final Date start;
+            private final Date end;
+            DateRange(Date start, Date end) {
+                this.start = start;
+                this.end = end;
+            }
+
+            public Date getStart() {
+                return start;
+            }
+
+            public Date getEnd() {
+                return end;
+            }
+
+            public boolean includes(Entry entry) {
+                return entry.getCharDate().equals(start) || entry.getCharDate().equals(end) || (entry.getCharDate().after(start) && entry
+                        .getCharDate().before(end));
+            }
+        }
+
+        class Account{
+            List<Entry> entryList = Lists.newArrayList();
+            double getFlowBetween(DateRange dateRange) {
+                double result = 0;
+                for (Entry entry : entryList) {
+                    if (entry.getCharDate().equals(dateRange.getStart()) || entry.getCharDate().equals(dateRange.getEnd()) || (
+                            entry.getCharDate().after(dateRange.getStart()) && entry.getCharDate().before(dateRange.getEnd()))) {
+                        result += entry.getValue();
+                    }
+                }
+                return result;
+            }
+
+            /////////方式2////////{@ling}////////////////////
+
+            /**
+             * @see refactoring.RfMoveMethod
+             */
+            double getFlowBetween2(DateRange dateRange) {
+                double result = 0;
+                for (Entry entry : entryList) {
+                    if (dateRange.includes(entry)) {
+                        result += entry.getValue();
+                    }
+                }
+                return result;
+            }
+        }
+
     }
 
 }
