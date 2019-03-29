896975a437f833b5bae3991e004e50c4c3277cc2
shangzhidong <shangzhidong@zhuanzhuan.com>
2019-03-29 13:21:17 +0800

    replace exception with test

--- src/main/java/refactoring/chp10_makingMethodCallsSimpler/RepalceExceptionWithTest.java
+++ src/main/java/refactoring/chp10_makingMethodCallsSimpler/RepalceExceptionWithTest.java
@@ -1,8 +1,18 @@
 package refactoring.chp10_makingMethodCallsSimpler;
 
+import com.sun.deploy.security.WrapResource;
+import sun.misc.Resource;
+import thecompletereferenc.EmptyArrayException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayDeque;
+
 /**
  * 当存在一个条件可以通过程序判断的方式来处理的时候，不要用异常的方式来实现
  *
+ *   exceptions should be used for exceptional behaviour-behaviour that is an unexpected error;
  *
  * double getValueForPeriod(int periodNumber) {
  *     try{
@@ -18,4 +28,39 @@ package refactoring.chp10_makingMethodCallsSimpler;
  * }
  */
 public class RepalceExceptionWithTest {
+
+    private ArrayDeque<Resource> available;
+    private ArrayDeque<Resource> allocated;
+
+    class Before {
+
+        class ResourcePool {
+            Resource  getResource(){
+                sun.misc.Resource result;
+                try {
+                    result = available.pop();
+                    allocated.push(result);
+                } catch (Exception e) {
+                    result = new WrapResource(null);
+                    allocated.push(result);
+                }
+                return result;
+            }
+        }
+    }
+    class After {
+
+        class ResourcePool {
+            Resource  getResource(){
+                sun.misc.Resource result;
+                if (available.isEmpty()) {
+                    result = new WrapResource(null);
+                }else {
+                    result = available.pop();
+                }
+                allocated.push(result);
+                return result;
+            }
+        }
+    }
 }
