6638b5b150defe5205fd528a3ed74e4e5c7b542a
shangzhidong <shangzhidong@zhuanzhuan.com>
2019-04-03 18:25:32 +0800

    pull up field‘”

---
+++ src/main/java/refactoring/chp11_dealingwithgeneralization/PullUpField.java
@@ -0,0 +1,14 @@
+package refactoring.chp11_dealingwithgeneralization;
+
+/**
+ * 两个子类有同一个成员field
+ *  将这个field 上移到父类中
+ *
+ *
+ *
+ * @author shangzhidong@zhuanzhuan.com
+ * @date 2019-04-02 13:11
+ */
+public class PullUpField {
+
+}
