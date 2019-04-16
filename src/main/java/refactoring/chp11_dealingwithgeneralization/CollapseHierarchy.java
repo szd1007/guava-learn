e2142ff21fd50ec6853e9e89f6926f030b1e7086
normalAdam <szd1007@gmail.com>
2019-04-16 08:34:00 +0800

    collapseHierarchy

---
+++ src/main/java/refactoring/chp11_dealingwithgeneralization/CollapseHierarchy.java
@@ -0,0 +1,11 @@
+package refactoring.chp11_dealingwithgeneralization;
+
+/**
+ * 父类和子类没啥太大区别， 将他们合并
+ *
+ * 继承之后子类并没有带来应有的价值，就要把子类或者父类去掉。
+ * 继承本来就会导致类结构过度复杂。
+ *
+ */
+public class CollapseHierarchy {
+}
---
+++ src/main/java/refactoring/chp11_dealingwithgeneralization/RfExtractInterface.java
@@ -0,0 +1,12 @@
+package refactoring.chp11_dealingwithgeneralization;
+
+/**
+ * 若干客户使用类接口中的同一个子集，或者两个类都接口有部分相同。
+ * 将相同的子集提炼到一个独立接口中
+ */
+public class RfExtractInterface {
+     //用接口代替具体的类交互。
+
+
+
+}
