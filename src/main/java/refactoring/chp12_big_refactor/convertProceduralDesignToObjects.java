82e965eaa8401f49777a983098cb7744932cb632
normalAdam <szd1007@gmail.com>
2019-05-01 21:31:23 +0800

    convert procedural design to objects

---
+++ src/main/java/refactoring/chp12_big_refactor/convertProceduralDesignToObjects.java
@@ -0,0 +1,10 @@
+package refactoring.chp12_big_refactor;
+
+/**
+ * 将过程化设计转化为对象设计
+ * 你手上有一些传统过程化风格的代码
+ * 将数据记录变成对象，将大块的行分成小块。并将行为移入相关对象之中。
+ *
+ */
+public class convertProceduralDesignToObjects {
+}
