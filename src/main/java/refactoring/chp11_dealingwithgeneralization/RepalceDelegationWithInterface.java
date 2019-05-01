ed9f5a62f651acc3a1e71afae9a89507ab683c0e
normalAdam <szd1007@gmail.com>
2019-05-01 15:09:13 +0800

    repalceInterfaceOrDelegation

---
+++ src/main/java/refactoring/chp11_dealingwithgeneralization/RepalceDelegationWithInterface.java
@@ -0,0 +1,9 @@
+package refactoring.chp11_dealingwithgeneralization;
+
+/**
+ * 要满足你会使用继承类中的所有函数，
+ * 如果存在共同的方法，你可以使用{@link RfExtractSuperClass} 来将共同部分提取出来。继承他们的父类
+ *
+ */
+public class RepalceDelegationWithInterface {
+}
---
+++ src/main/java/refactoring/chp11_dealingwithgeneralization/ReplaceInterfaceWithDelegation.java
@@ -0,0 +1,10 @@
+package refactoring.chp11_dealingwithgeneralization;
+
+/**
+ * 以委托取代继承。（组合取代继承）
+ * 某个子类只使用超类接口中的一部分，或是根本不需要继承带来的数据。
+ *
+ * 在子类中新建一个字段用以保存超类；调整子类函数，令它改而委托超类；然后去掉两者之间的继承关系。
+ */
+public class ReplaceInterfaceWithDelegation {
+}
