7d60f2c0def54401b8bf592e8c2d1cc6ebd1548a
normalAdam <szd1007@gmail.com>
2019-05-28 09:18:00 +0800

    bridge

---
+++ src/main/java/designPattern/chp4_structural_patterns/aBridge42.java
@@ -0,0 +1,25 @@
+package designPattern.chp4_structural_patterns;
+
+/**
+ * 桥接模式
+ *
+ * 1 意图
+ *    将抽象部分与它的实现部分分离， 使它们都可以独立地变化。
+ * 2 别名  Handle/Body
+ * 3
+ * 4 适用性
+ *   1你不想在抽象和它的实现部分之间有一个固定的绑定关系。例如你想在运行时刻可以将实现部分进行选择切换
+ *   2类抽象和实现部分都可以生成子类的方式进行扩充。该模式可以对不同的实现部分和抽象接口进行组合。
+
+
+
+   结构
+ https://note.youdao.com/yws/public/resource/f8d17e644e954eff94da0ffe8331f9f7/xmlnote/WEBRESOURCE6940ed193f9a2da7df719e099257e3b4/21313
+ https://note.youdao.com/yws/public/resource/f8d17e644e954eff94da0ffe8331f9f7/xmlnote/WEBRESOURCE6940ed193f9a2da7df719e099257e3b4/21313
+
+ 有点类似模板类方式，但是把模板类中的抽象方法提炼成接口采用外部实现。方便适用性1
+
+
+ */
+public class aBridge42 {
+}
