f72e3696f27681b3cfc560f399ac7bb645d9beba
shangzhidong <shangzhidong@zhuanzhuan.com>
2019-05-31 16:32:58 +0800

    up

---
+++ src/main/java/concurrency/LockDemo.java
@@ -0,0 +1,30 @@
+package concurrency;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author shangzhidong@zhuanzhuan.com
+ * @date 2019-05-31 15:49
+ */
+public class LockDemo {
+
+    private static ReentrantLock lock = new ReentrantLock();
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 5; i++) {
+            Thread.sleep(10);
+            Thread t = new Thread(() -> {
+                lock.lock();
+                try {
+                    Thread.sleep(1000000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+
+            });
+            t.start();
+        }
+        System.out.println("init done");
+    }
+}
