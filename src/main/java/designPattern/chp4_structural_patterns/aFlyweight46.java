021481b69fa92730161db345499ffac20258b392
shangzhidong <shangzhidong@zhuanzhuan.com>
2019-05-31 15:46:36 +0800

    flyweight46

---
+++ src/main/java/designPattern/chp4_structural_patterns/aFlyweight46.java
@@ -0,0 +1,12 @@
+package designPattern.chp4_structural_patterns;
+
+/**
+ * 享元
+ * 1 意图   运用共享技术有效地支持大量细粒度的对象
+ *
+ * @author shangzhidong@zhuanzhuan.com
+ * @date 2019-05-30 11:19
+ */
+public class aFlyweight46 {
+
+}
