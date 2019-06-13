e22965957ca50e8f39146a897fef805c3eb0dc3e
shangzhidong <shangzhidong@zhuanzhuan.com>
2019-06-13 13:21:27 +0800

    迭代器

---
+++ src/main/java/designPattern/chp5_behavior_patterns/aInterpreter53.java
@@ -0,0 +1,12 @@
+package designPattern.chp5_behavior_patterns;
+
+/**
+ * 解释器
+ * 1 意图  给定一个语言，定义它的文法的一种表示，并定义一个解释器，这个解释器使用该表示来解释语言中的句子。
+ * 2 动机
+ * @author shangzhidong@zhuanzhuan.com
+ * @date 2019-06-13 13:05
+ */
+public class aInterpreter53 {
+
+}
---
+++ src/main/java/designPattern/chp5_behavior_patterns/aIterator54.java
@@ -0,0 +1,11 @@
+package designPattern.chp5_behavior_patterns;
+
+/**
+ * 迭代器模式   （别名  游标，cursor)
+ *
+ * 1 意图
+ *    提供一种方法顺序访问一个聚合对象中的各个元素，而又不需暴露该对象的内部表示。
+ */
+public class aIterator54 {
+
+}
