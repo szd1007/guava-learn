f45aee06bb1bfad51fd65550ed4d58f066ea6179
normalAdam <szd1007@gmail.com>
2019-06-19 09:12:21 +0800

    up

---
+++ src/main/java/designPattern/chp5_behavior_patterns/aStrategy59.java
@@ -0,0 +1,12 @@
+package designPattern.chp5_behavior_patterns;
+
+/**
+ * 策略  别名 政策（policy）
+ *
+ * 1 意图  定义一系列的算法， 把它们一个个封装起来，并且使它们可相互替换。本模式使得算法可独立于
+ * 使用它的客户而变化
+ *
+ *
+ */
+public class aStrategy59 {
+}
