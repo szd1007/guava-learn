f26138867e929377b221a5dcb76567de8adcee23
normalAdam <szd1007@gmail.com>
2019-06-19 08:50:14 +0800

    state machine

---
+++ src/main/java/designPattern/chp5_behavior_patterns/aState58.java
@@ -0,0 +1,12 @@
+package designPattern.chp5_behavior_patterns;
+
+/**
+ *   状态  别名（状态对象  Objects of States）
+ *
+ *   1 意图  允许一个对象在其内部状态改变时改变它的行为。对象看起来似乎是修改了它的类。
+ *
+ *   实现  基于表的状态机配置模式
+ *
+ */
+public class aState58 {
+}
