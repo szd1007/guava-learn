5d8406ca2e3e2040283c81b553a37d7fdc3a07dd
shangzhidong <shangzhidong@zhuanzhuan.com>
2019-06-21 13:40:18 +0800

    up

--- src/main/java/effectiveJava/Eff_2.java
+++ src/main/java/effectiveJava/Eff_2.java
@@ -1,5 +1,9 @@
 package effectiveJava;
 
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
+
 /**
  * 2. 构造器参数较多时考虑使用构建器[builder]
  * <p>
@@ -9,13 +13,16 @@ package effectiveJava;
  */
 @SuppressWarnings("all")
 public class Eff_2 {
+
     public static void main(String[] args) {
         NutritionFacts.Builder builder = new NutritionFacts.Builder(1, 3);
         builder.sodium(33).calories(134).build();
     }
 }
+
 @SuppressWarnings("all")
- class NutritionFacts {
+class NutritionFacts {
+
     private final int servingSize;
     private final int servings;
     private final int calories;
@@ -24,8 +31,9 @@ public class Eff_2 {
     private final int carbohydrate;
 
     public static class Builder {
+
         // Required parameters private final int servingSize; private final int servings;
-        //必备参数，里面再声明一遍变量应该就是为了使外部final可以多次使用
+        @EfLanguagePoints("必备参数，里面再声明一遍变量应该就是为了使外部final可以多次使用")
         private final int servingSize;
         private final int servings;
         //可选参数 给默认值
@@ -77,8 +85,64 @@ public class Eff_2 {
     }
 }
 
+@EfLanguagePoints("类结构体系中的builder使用")
+abstract class Pizza {
+
+    public enum Topping {HAM, MUSHROOM, ONION, PEPPER, SAUSAGE}
+
+    ;
+    final Set<Topping> toppings;
+
+    @EfLanguagePoints("<T extends Bulider<T>> 是父类方法也可以返回子类")
+    abstract static class Bulider<T extends Bulider<T>> {
+
+        EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
+
+        public T addTopping(Topping topping) {
+            toppings.add(Objects.requireNonNull(topping));
+            return self();
+        }
+
+        protected abstract T self();
+
+        abstract Pizza build();
+    }
+
+    Pizza(Bulider<?> bulider) {
+        toppings = bulider.toppings.clone();
+    }
+}
+ class NyPizza extends Pizza{
+
+    public NyPizza(SubBuilder builder) {
+        super(builder);
+        this.size = builder.size;
+    }
+
+    public enum Size {SMALL, MEDIUM, LARGE};
+    private final Size size;
+
+    public static class SubBuilder extends Pizza.Bulider<SubBuilder>{
+        private final Size size;
+        public SubBuilder(Size size) {
+            this.size = Objects.requireNonNull(size);
+        }
+
+        @Override
+        protected SubBuilder self() {
+            return this;
+        }
+
+        @Override
+        @EfLanguagePoints("子类重写的方法返回值可以改变成原来类的子类")
+        NyPizza build() {
+            return new NyPizza(this);
+        }
+    }
+}
 
 class NutritionFacts2 {
+
     private final int servingSize; // (mL) required private final int servings; // (per container) required
     private final int servings;
     private final int calories;
@@ -105,8 +169,7 @@ class NutritionFacts2 {
         this(servingSize, servings, calories, fat, sodium, 0);
     }
 
-    public NutritionFacts2(int servingSize, int servings,
-                           int calories, int fat, int sodium, int carbohydrate) {
+    public NutritionFacts2(int servingSize, int servings, int calories, int fat, int sodium, int carbohydrate) {
         this.servingSize = servingSize;
         this.servings = servings;
         this.calories = calories;
