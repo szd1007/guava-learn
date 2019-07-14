5167f7b3413d32ecf8e79ed882f085de46434730
shangzhidong <shangzhidong@zhuanzhuan.com>
2019-07-14 18:01:47 +0800

    done enum

--- src/main/java/effectiveJava/EfLanguagePoints.java
+++ src/main/java/effectiveJava/EfLanguagePoints.java
@@ -1,9 +1,6 @@
 package effectiveJava;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 import java.util.EnumSet;
 
 import static java.lang.annotation.ElementType.*;
@@ -16,7 +13,13 @@ import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
  */
 @Target({TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, LOCAL_VARIABLE, ANNOTATION_TYPE, TYPE_USE})
 @Retention(RetentionPolicy.SOURCE)
+@Repeatable(EfLanguagePointsContainer.class)
 public @interface EfLanguagePoints {
     String[] value() default "";
     Class[] values() default Object.class;
+}
+@Target({TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, LOCAL_VARIABLE, ANNOTATION_TYPE, TYPE_USE})
+@Retention(RetentionPolicy.SOURCE)
+@interface EfLanguagePointsContainer {
+    EfLanguagePoints[] value() ;
 }
