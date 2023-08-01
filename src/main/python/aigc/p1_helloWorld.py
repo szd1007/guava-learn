7e72bd876f07c784ae99a6190f5f53842c042473
shangzhidong <shangzhidong@zhuanzhuan.com>
2023-08-01 08:55:22 +0800

    差评好评分析

--- src/main/python/aigc/p1_helloWorld.py
+++ src/main/python/aigc/p1_helloWorld.py
@@ -5,21 +5,21 @@ import os
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 COMPLETION_MODEL = "text-davinci-003"
 
-#prompt = """
-#Consideration product : 工厂现货PVC充气青蛙夜市地摊热卖充气玩具发光蛙儿童水上玩具
-
-#1. Compose human readable product title used on Amazon in english within 20 words.
-#2. Write 5 selling points for the products in Amazon.
-#3. Evaluate a price range for this product in U.S.
-
-#Output the result in json format with three properties called title, selling_points and price_range
-#"""
-
 prompt = """
-Man Utd must win trophies, says Ten Hag ahead of League Cup final
+Consideration product : 工厂现货PVC充气青蛙夜市地摊热卖充气玩具发光蛙儿童水上玩具
 
-请将上面这句话的人名提取出来，并用json的方式展示出来
+1. Compose human readable product title used on Amazon in english within 20 words.
+2. Write 5 selling points for the products in Amazon.
+3. Evaluate a price range for this product in U.S.
+
+Output the result in json format with three properties called title, selling_points and price_range
 """
+
+# prompt = """
+# Man Utd must win trophies, says Ten Hag ahead of League Cup final
+#
+# 请将上面这句话的人名提取出来，并用json的方式展示出来
+# """
 
 
 def get_response(prompt):
